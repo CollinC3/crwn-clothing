0976d790501c563d9fd6bb93c22e5c67bc92023f
CollinC3 <collinc1997@gmail.com>
2021-09-08 20:09:08 -0700

    Added a sign-in page, form, and button

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in';
+
+import './sign-in-sign-up.scss';
+
+const SignInSignUpPage = () => (
+    <div className='sign-in-sign-up'>SIGN IN
+    <SignIn/>
+    </div>
+);
+
+export default SignInSignUpPage;
